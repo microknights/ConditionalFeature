82b75b0aa1ffe34cfc1a58c29bb9ed9b40c51f48
Frank L. Nielsen <fln@microknights.dk>
2017-10-25 00:16:13 +0200

    test now complete successfully

---
+++ test/TestBooleanConditionalFeatures.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using MicroKnights.ConditionalFeature.Configuation;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace MicroKnights.ConditionalFeature.Test
+{
+    public class TestBooleanConditionalFeatures
+    {
+        private class MyCustomBooleanConditionalTrueFeature : BooleanConditionalFeature
+        {
+            public MyCustomBooleanConditionalTrueFeature() 
+            {}
+
+            public MyCustomBooleanConditionalTrueFeature(Func<bool> funcResolveValue, bool defaultValue = default(bool)) : base(funcResolveValue, defaultValue)
+            {}
+
+            protected override bool ResolveFeatureValue()
+            {
+                return true;
+            }
+        }
+        private class MyCustomBooleanConditionalFalseFeature : BooleanConditionalFeature
+        {
+            public MyCustomBooleanConditionalFalseFeature() 
+            {}
+
+            public MyCustomBooleanConditionalFalseFeature(Func<bool> funcResolveValue, bool defaultValue = default(bool)) : base(funcResolveValue, defaultValue)
+            {
+            }
+
+            protected override bool ResolveFeatureValue()
+            {
+                return false;
+            }
+        }
+
+        private class MyConfigurationRootTrueBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationRootTrueBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationRootFalseBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationRootFalseBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationRootFormatExceptionBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationRootFormatExceptionBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+
+        private class MyConfigurationSectionTrueBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationSectionTrueBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationSectionFalseBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationSectionFalseBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationSectionFormatExceptionBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationSectionFormatExceptionBooleanFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+
+        private class MyConfigurationMissingDefaultToTrueBooleanFeature : BooleanConditionalConfigurationFeature
+        {
+            public MyConfigurationMissingDefaultToTrueBooleanFeature(IConfiguration configuration, bool defaultValue)
+                : base(configuration, defaultValue)
+            { }
+        }
+
+        [Fact]
+        public void TestCustomOverrideFeatures()
+        {
+            var customTrue = new MyCustomBooleanConditionalTrueFeature();
+            Assert.True(customTrue.IsEnabled, $"{customTrue.GetType().Name} failed");
+
+            var customFalse = new MyCustomBooleanConditionalFalseFeature();
+            Assert.True(customFalse.IsDisabled, $"{customFalse.GetType().Name} failed");
+        }
+
+        [Fact]
+        public void TestCustomDelegateFeatures()
+        {
+            var customDelegateTrue = new MyCustomBooleanConditionalFalseFeature(() => true);
+            Assert.True(customDelegateTrue.IsEnabled, $"{customDelegateTrue.GetType().Name} delegate failed");
+
+            var customDelegateFalse = new MyCustomBooleanConditionalTrueFeature(() => false);
+            Assert.True(customDelegateFalse.IsDisabled, $"{customDelegateFalse.GetType().Name} delegate failed");
+        }
+
+        [Fact]
+        public void TestAppSettingsFeatures()
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(GetType().Assembly.Location))
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+
+            var rootTrue = new MyConfigurationRootTrueBooleanFeature(config);
+            Assert.True(rootTrue.IsEnabled, $"{rootTrue.GetType().Name} failed");
+
+            var rootFalse = new MyConfigurationRootFalseBooleanFeature(config);
+            Assert.True(rootFalse.IsDisabled, $"{rootFalse.GetType().Name} failed");
+
+            var rootFormatException = new MyConfigurationRootFormatExceptionBooleanFeature(config);
+            Assert.Throws<FormatException>(() => rootFormatException.IsEnabled);
+
+
+            var sectionTrue = new MyConfigurationSectionTrueBooleanFeature(config);
+            Assert.True(sectionTrue.IsEnabled, $"{sectionTrue.GetType().Name} failed");
+
+            var sectionFalse = new MyConfigurationSectionFalseBooleanFeature(config);
+            Assert.True(sectionFalse.IsDisabled, $"{sectionFalse.GetType().Name} failed");
+
+            var sectionFormatException = new MyConfigurationSectionFormatExceptionBooleanFeature(config);
+            Assert.Throws<FormatException>(() => sectionFormatException.IsEnabled);
+
+            var missingDefaultTrue = new MyConfigurationMissingDefaultToTrueBooleanFeature(config, true);
+            Assert.True(missingDefaultTrue.IsEnabled, $"{missingDefaultTrue.GetType().Name} failed");
+        }
+    }
+}
---
+++ test/TestEnumConditionalFeatures.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using MicroKnights.ConditionalFeature.Configuation;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace MicroKnights.ConditionalFeature.Test
+{
+    public class TestEnumConditionalFeatures
+    {
+        private enum MyColorEnum
+        {
+            Red,
+            Green,
+            Blue,
+            Transparent
+        }
+
+        private class MyCustomEnumConditionalRedFeature : EnumConditionalFeature<MyColorEnum>
+        {
+            public MyCustomEnumConditionalRedFeature(MyColorEnum defaultValue = default(MyColorEnum)) : base(defaultValue)
+            {
+            }
+
+            public MyCustomEnumConditionalRedFeature(Func<MyColorEnum> funcResolveValue, MyColorEnum defaultValue = default(MyColorEnum)) : base(funcResolveValue, defaultValue)
+            {
+            }
+
+            protected override MyColorEnum ResolveFeatureValue()
+            {
+                return MyColorEnum.Red;
+            }
+        }
+
+        private class MyCustomEnumConditionalGreenFeature : EnumConditionalFeature<MyColorEnum>
+        {
+            public MyCustomEnumConditionalGreenFeature(MyColorEnum defaultValue = default(MyColorEnum)) : base(defaultValue)
+            {
+            }
+
+            public MyCustomEnumConditionalGreenFeature(Func<MyColorEnum> funcResolveValue, MyColorEnum defaultValue = default(MyColorEnum)) : base(funcResolveValue, defaultValue)
+            {
+            }
+
+            protected override MyColorEnum ResolveFeatureValue()
+            {
+                return MyColorEnum.Green;
+            }
+        }
+
+
+        private class MyConfigurationRootRedEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationRootRedEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationRootGreenEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationRootGreenEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationRootFormatExceptionEnumFeature :  EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationRootFormatExceptionEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+
+        private class MyConfigurationSectionRedEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationSectionRedEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationSectionGreenEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationSectionGreenEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+        private class MyConfigurationSectionFormatExceptionEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationSectionFormatExceptionEnumFeature(IConfiguration configuration)
+                : base(configuration)
+            { }
+        }
+
+        private class MyConfigurationMissingDefaultToTransparentEnumFeature : EnumConditionalConfigurationFeature<MyColorEnum>
+        {
+            public MyConfigurationMissingDefaultToTransparentEnumFeature(IConfiguration configuration, MyColorEnum defaultValue)
+                : base(configuration, defaultValue)
+            { }
+        }
+
+
+        [Fact]
+        public void TestCustomOverrideFeatures()
+        {
+            var customGreen = new MyCustomEnumConditionalGreenFeature();
+            Assert.True(customGreen.Is(MyColorEnum.Green), $"{customGreen.GetType().Name} failed");
+
+            var customRed = new MyCustomEnumConditionalRedFeature();
+            Assert.True(customRed.Is(MyColorEnum.Red), $"{customRed.GetType().Name} failed");
+        }
+
+        [Fact]
+        public void TestCustomDelegateFeatures()
+        {
+            var customDelegateGreen = new MyCustomEnumConditionalRedFeature(() => MyColorEnum.Green);
+            Assert.True(customDelegateGreen.Is(MyColorEnum.Green), $"{customDelegateGreen.GetType().Name} delegate failed");
+
+            var customDelegateRed = new MyCustomEnumConditionalGreenFeature(() => MyColorEnum.Red);
+            Assert.True(customDelegateRed.Is(MyColorEnum.Red), $"{customDelegateRed.GetType().Name} delegate failed");
+        }
+
+        [Fact]
+        public void TestAppSettingsFeatures()
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(GetType().Assembly.Location))
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+
+            var rootGreen = new MyConfigurationRootGreenEnumFeature(config);
+            Assert.True(rootGreen.Is(MyColorEnum.Green), $"{rootGreen.GetType().Name} failed");
+            Assert.True(rootGreen.Not(MyColorEnum.Blue), $"{rootGreen.GetType().Name} failed");
+
+            var rootRed = new MyConfigurationRootRedEnumFeature(config);
+            Assert.True(rootRed.Is(MyColorEnum.Red), $"{rootRed.GetType().Name} failed");
+            Assert.False(rootRed.Is(MyColorEnum.Blue), $"{rootRed.GetType().Name} failed");
+            Assert.True(rootRed.Any(MyColorEnum.Blue, MyColorEnum.Red), $"{rootRed.GetType().Name} failed");
+            Assert.True(rootRed.NotAny(MyColorEnum.Blue, MyColorEnum.Green), $"{rootRed.GetType().Name} failed");
+
+            var rootFormatException = new MyConfigurationRootFormatExceptionEnumFeature(config);
+            Assert.Throws<FormatException>(()=>rootFormatException.Is(MyColorEnum.Transparent));
+
+
+            var sectionGreen = new MyConfigurationSectionGreenEnumFeature(config);
+            Assert.True(sectionGreen.Is(MyColorEnum.Green), $"{sectionGreen.GetType().Name} failed");
+
+            var sectionRed = new MyConfigurationSectionRedEnumFeature(config);
+            Assert.True(sectionRed.Is(MyColorEnum.Red), $"{sectionRed.GetType().Name} failed");
+
+            var sectionFormatException = new MyConfigurationSectionFormatExceptionEnumFeature(config);
+            Assert.Throws<FormatException>(() => sectionFormatException.Is(MyColorEnum.Transparent));
+
+            var missingDefaultTransparent = new MyConfigurationMissingDefaultToTransparentEnumFeature(config, MyColorEnum.Transparent);
+            Assert.True(missingDefaultTransparent.Is(MyColorEnum.Transparent), $"{missingDefaultTransparent.GetType().Name} failed");
+        }
+    }
+}
